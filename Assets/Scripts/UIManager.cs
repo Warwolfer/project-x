4441113e55e35e0bd6e7cdb88c0e3e97a31ec352
Kenny Reynaldo <warwolfer@yahoo.com>
2019-04-26 15:35:37 +0700

    Main Menu base done

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 public class UIManager : MonoBehaviour
 {
     public Image[] healths;
@@ -13,10 +13,13 @@ public class UIManager : MonoBehaviour
     public GameObject[] optionMenu;
     public GameObject[] finishMenu;
     public Text scoreText;
+    CanvasGroup canvasGroup;
+
     // Use this for initialization
     void Start()
     {
-
+        canvasGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(DoFadeIn());
     }
 
     void Update()
@@ -197,4 +200,54 @@ public class UIManager : MonoBehaviour
         AudioListener.volume = GameObject.Find("VolumeSlider").GetComponent<Slider>().value;
     }
 
+    public void startGame()
+    {
+        StartCoroutine(StartGameFade());
+    }
+
+    IEnumerator StartGameFade()
+     {
+         yield return DoFadeOut();
+         StartCoroutine(LoadYourAsyncScene("SampleScene"));
+     }
+
+    IEnumerator DoFadeOut()
+     {
+         float elapsedTime = 0;
+         float fadeTime = 2f;
+         while(canvasGroup.alpha > 0)
+         {
+             elapsedTime += Time.deltaTime;
+             canvasGroup.alpha = Mathf.Clamp01(1.0f - (elapsedTime / fadeTime));
+             yield return null;
+         }
+     }
+
+     IEnumerator DoFadeIn()
+     {
+         float elapsedTime = 0;
+         float fadeTime = 2f;
+         while(canvasGroup.alpha < 1)
+         {
+             elapsedTime += Time.deltaTime;
+             canvasGroup.alpha = Mathf.Clamp01(0 + (elapsedTime / fadeTime));
+             yield return null;
+         }
+     }
+
+     IEnumerator LoadYourAsyncScene(string scene)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
 }
